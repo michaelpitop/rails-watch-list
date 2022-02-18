86215f6dd8ffcf4787406d282f452456b5e8a5ee
Michael	Pitopoulakis michaelpitop2@gmail.com <michaelpitop2@gmail.com>
2022-02-18 15:12:24 +0100

    fml

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,5 +1,6 @@
 class BookmarksController < ApplicationController
-  before_action :find_list, only: [ :new, :create ]
+  before_action :set_bookmark, only: :destroy
+  before_action :set_request, only: [:new, :create]
 
   def new
     @bookmark = Bookmark.new
@@ -9,25 +10,28 @@ class BookmarksController < ApplicationController
     @bookmark = Bookmark.new(bookmark_params)
     @bookmark.list = @list
     if @bookmark.save
-      redirect_to list_path(@lists)
+      redirect_to list_path(@list)
     else
-      render template: "list/show"
+      render :new
     end
   end
 
   def destroy
-    @bookmark = Bookmark.find(params[:id])
     @bookmark.destroy
-    redirect_to root_path
+    redirect_to list_path(@bookmark.list)
   end
 
   private
 
-  def find_list
-    @list = List.find(params[:list_id])
-  end
-
   def bookmark_params
     params.require(:bookmark).permit(:comment, :movie_id)
+  end
+
+  def set_bookmark
+    @bookmark = Bookmark.find(params[:id])
+  end
+
+  def set_request
+    @list = List.find(params[:list_id])
   end
 end
--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,11 +1,11 @@
 class ListsController < ApplicationController
+  before_action :set_list, only: [:show, :destroy]
 
   def index
-    @list = List.all
+    @lists = List.all
   end
 
   def show
-    @list = List.find(params[:id])
     @bookmark = Bookmark.new
   end
 
@@ -18,17 +18,20 @@ class ListsController < ApplicationController
     if @list.save
       redirect_to list_path(@list)
     else
-      render 'new'
+      render :new
     end
   end
 
   def destroy
-    @list = List.find(params[:id])
     @list.destroy
-    redirect_to root_path
+    redirect_to lists_path
   end
 
   private
+  
+  def set_list
+    @list = List.find(params[:id])
+  end
 
   def list_params
     params.require(:list).permit(:name)
